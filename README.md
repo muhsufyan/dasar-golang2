8931d8cba049a1215c71e1bb292d6d3c7cea51f3
muhsufyan <muh.sufyan.ts@gmail.com>
2022-08-05 12:59:56 +0700

    finish but need refactor

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 implementasi controller
 
 NOTE BARU BUAT FUNC IMPLEMENTASINYA BLM ADA ISINYA
+
+kita buat data model untuk standard response di model/web/web_response.go dimana data model ini akan teruhubung ke user, sedangkan model/web/category_response.go as data model response dr service (ini agar tdk tertukar pengertiannya sekaligus meralat seblmnya jika salah)
+
+NOTE : IMPLEMENTASI CONTROLLER CATEGORY SDH SELESAI TP PERLU MELAKUKAN REFACTOR KARENA BANYAK KODE YG DUPLICATE(MENGULAH" KODE YG SAMA)
--- controller/category_controller_imp.go
+++ controller/category_controller_imp.go
@@ -1,10 +1,14 @@
 package controller
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
 
-	"github.com/go-delve/delve/service"
 	"github.com/julienschmidt/httprouter"
+	"github.com/muhsufyan/dasar-golang2/helper"
+	"github.com/muhsufyan/dasar-golang2/model/web"
+	"github.com/muhsufyan/dasar-golang2/service"
 )
 
 type CategoryControllerImp struct {
@@ -13,9 +17,107 @@ type CategoryControllerImp struct {
 }
 
 func (controller *CategoryControllerImp) Create(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-	// baca json
+	// baca json, karena request.Body mrpkn io.Read maka kita bisa stream datanya memakai decoder
+	decoder := json.NewDecoder(request.Body)
+	categoryCreateRequest := web.CategoryCreateRequest{}
+	// decode data
+	err := decoder.Decode(&categoryCreateRequest)
+	helper.PanicIfError(err)
+	categoryResponse := controller.CategoryService.Create(request.Context(), categoryCreateRequest)
+	// data model untuk response ke user
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "Ok",
+		Data:   categoryResponse,
+	}
+	// tambah header
+	writer.Header().Add("Content-Type", "Application/json")
+	// convert data jd json as response data ke user
+	encoder := json.NewEncoder(writer)
+	err = encoder.Encode(webResponse)
+	helper.PanicIfError(err)
 }
-func (controller *CategoryControllerImp) Update(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
-func (controller *CategoryControllerImp) Delete(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
-func (controller *CategoryControllerImp) FindById(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
-func (controller *CategoryControllerImp) FindAll(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
+func (controller *CategoryControllerImp) Update(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	// update sama sprti create diatas bedanya kita perlu tahu dulu id berapa yg diupdate
+	// baca json, karena request.Body mrpkn io.Read maka kita bisa stream datanya memakai decoder
+	decoder := json.NewDecoder(request.Body)
+	categoryUpdateRequest := web.CategoryUpdateRequest{}
+	// decode data
+	err := decoder.Decode(&categoryUpdateRequest)
+	helper.PanicIfError(err)
+	// get id yg ingin diupdate melalui params.ByName() yg return string jd kita perlu convert jd int
+	categoryId := params.ByName("categoryId")
+	// convert id from str to int
+	id, err := strconv.Atoi(categoryId)
+	helper.PanicIfError(err)
+	categoryUpdateRequest.Id = id
+	categoryResponse := controller.CategoryService.Update(request.Context(), categoryUpdateRequest)
+	// data model untuk response ke user
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "Ok",
+		Data:   categoryResponse,
+	}
+	// tambah header
+	writer.Header().Add("Content-Type", "Application/json")
+	// convert data jd json as response data ke user
+	encoder := json.NewEncoder(writer)
+	err = encoder.Encode(webResponse)
+	helper.PanicIfError(err)
+}
+func (controller *CategoryControllerImp) Delete(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	// get id yg ingin didelete melalui params.ByName() yg return string jd kita perlu convert jd int
+	categoryId := params.ByName("categoryId")
+	// convert id from str to int
+	id, err := strconv.Atoi(categoryId)
+	helper.PanicIfError(err)
+
+	controller.CategoryService.Delete(request.Context(), id)
+	// data model untuk response ke user
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "Ok",
+	}
+	// tambah header
+	writer.Header().Add("Content-Type", "Application/json")
+	// convert data jd json as response data ke user
+	encoder := json.NewEncoder(writer)
+	err = encoder.Encode(webResponse)
+	helper.PanicIfError(err)
+}
+func (controller *CategoryControllerImp) FindById(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	// get id yg ingin didelete melalui params.ByName() yg return string jd kita perlu convert jd int
+	categoryId := params.ByName("categoryId")
+	// convert id from str to int
+	id, err := strconv.Atoi(categoryId)
+	helper.PanicIfError(err)
+
+	categoryResponse := controller.CategoryService.FindById(request.Context(), id)
+	// data model untuk response ke user
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "Ok",
+		Data:   categoryResponse,
+	}
+	// tambah header
+	writer.Header().Add("Content-Type", "Application/json")
+	// convert data jd json as response data ke user
+	encoder := json.NewEncoder(writer)
+	err = encoder.Encode(webResponse)
+	helper.PanicIfError(err)
+}
+func (controller *CategoryControllerImp) FindAll(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	categoriesResponses := controller.CategoryService.FindAll(request.Context())
+	// data model untuk response ke user
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "Ok",
+		Data:   categoriesResponses,
+	}
+	// tambah header
+	writer.Header().Add("Content-Type", "Application/json")
+	// convert data jd json as response data ke user
+	encoder := json.NewEncoder(writer)
+	err := encoder.Encode(webResponse)
+	helper.PanicIfError(err)
+}
